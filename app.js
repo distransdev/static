43b7eda8f843ad921d60bbdebc6f8b9539c277b2
distransdev <mikkel@distrans.dev>
2024-07-06 18:34:12 +0200

    replace default page with animated starship

---
+++ app.js
@@ -0,0 +1,11 @@
+const images = [
+    ['https://cdn.esawebb.org/archives/images/large/pillarsofcreation_composite.jpg', 'NASA, ESA, CSA, STScI, J. DePasquale (STScI), A. Pagan (STScI), A. M. Koekemoer (STScI)'],
+    ['/carinanebula5.jpg', 'NASA, ESA, CSA, and STScI, J. DePasquale (STScI)'],
+    // ['https://cdn.esawebb.org/archives/images/large/potm2208a.jpg', 'ESA/Webb, NASA & CSA, J. Lee and the PHANGS-JWST Team. Acknowledgement: J. Schmidt']
+];
+const idx = Math.floor(Math.random() * images.length)
+const image = images[idx][0]
+const credit = images[idx][1]
+
+document.getElementById('space-image').style.backgroundImage=`url(${image})`;
+document.getElementById('image-credits').innerText="Background image credits: "+credit;
